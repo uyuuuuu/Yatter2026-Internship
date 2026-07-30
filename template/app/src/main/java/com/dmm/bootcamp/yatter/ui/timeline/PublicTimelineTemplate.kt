d7c1ccd3671681b86d59251ddb7a705d89f068f9
uyuuuuu <soufflesumire.03@gmail.com>
2026-07-30 13:44:50 +0900

    fix: importをmaterial3に修正

--- template/app/src/main/java/com/dmm/bootcamp/yatter/ui/timeline/PublicTimelineTemplate.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter/ui/timeline/PublicTimelineTemplate.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
@@ -15,6 +14,7 @@ import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
