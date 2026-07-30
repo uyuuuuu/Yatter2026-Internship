beb5d2d287bcf72116783738e4cb165ea19dbc4c
uyuuuuu <soufflesumire.03@gmail.com>
2026-07-30 15:09:34 +0900

    feat: スプラッシュ画面

--- template/app/src/main/java/com/dmm/bootcamp/yatter/MainActivity.kt
+++ template/app/src/main/java/com/dmm/bootcamp/yatter/MainActivity.kt
@@ -3,12 +3,14 @@ package com.dmm.bootcamp.yatter
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dmm.bootcamp.yatter.ui.MainApp
 import com.dmm.bootcamp.yatter.ui.theme.YatterTheme
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    installSplashScreen()
     super.onCreate(savedInstanceState)
 
     setContent {
